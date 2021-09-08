f5bc74e946fea2d7b612f3513a6b7df90bfde070
Samuel <samlee1097@yahoo.com>
2021-09-08 09:48:14 -0400

    commit tree fad43faced489a2f283c0c0908a137a1d4d1a624

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
 console.log('%c HI', 'color: firebrick')
+
+//Challenge 1
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+document.addEventListener("DOMContentLoaded", () => {
+    addDogImage()
+    addDogBreed()
+});
+
+function addDogImage(){
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach(url => {
+        const newImage = document.createElement("img");
+        newImage.src = url;
+        document.querySelector("#dog-image-container").append(newImage);
+        });
+    });
+}
+
+//Challenge 2
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+function addDogBreed() {
+    fetch(breedUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(data => checkBreeds(data));
+};
+
+function checkBreeds(data){
+    for(dataBreed in data.message){
+        const newDogBreed = document.createElement("li");
+        newDogBreed.id = dataBreed;
+        newDogBreed.className = "dog-names";
+        newDogBreed.innerText = `${dataBreed}`;
+        dogNameChange(newDogBreed);
+
+        document.querySelector("#dog-breeds").append(newDogBreed);
+        if (data.message[dataBreed].length !== 0){
+            const subBreedCategory = document.createElement("ul");
+            data.message[dataBreed].forEach(function(specificBreed) {
+                const subBreedNames = document.createElement("li")
+                subBreedNames.innerText = specificBreed;
+                subBreedCategory.append(subBreedNames);
+            });
+            newDogBreed.append(subBreedCategory);
+        }
+    };
+};
+
+//Challenge 3
+function dogNameChange(newDogBreed){
+    newDogBreed.addEventListener('click', function() {
+        if(newDogBreed.style.color !== "green"){
+            newDogBreed.style.color = "green";
+        } else{
+            newDogBreed.style.color = "black";
+        }
+    });
+};
